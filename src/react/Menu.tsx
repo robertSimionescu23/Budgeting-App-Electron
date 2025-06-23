e813a975feba72b580e4a756d851566ffba61c2b
robertSimionescu23 <simionescurobert23@gmail.com>
2025-06-23 21:42:19 +0300

    Added date verification. Added way to add year tabs. Need work on setting up the api to make files for each year.

--- src/react/Menu.tsx
+++ src/react/Menu.tsx
@@ -4,8 +4,56 @@ import { ChevronLeftIcon, PlusIcon } from '@heroicons/react/24/solid';
 import YearTab from './YearTab';
 
 function Menu() {
+    const [currentEntry, setCurrentEntry] = useState<{ day: number; month: number; year: number }>({
+        day: 0,
+        month: 0,
+        year: 0
+    });
+
+    const [Date, setDate] = useState<{ day: number; month: number; year: number }>();
     const [isFullSize, setIsFullSize] = useState(true);
     const [chooseYear, setChooseYear] = useState(false);
+    const [yearTabs, setYearTabs] = useState<number[]>([]);
+
+    function handleSubmit(e: React.FormEvent) {
+        e.preventDefault();
+
+        setCurrentEntry({day: currentEntry.day === 0 ? -1 : currentEntry.day,
+                         month: currentEntry.month === 0 ? -1 : currentEntry.month,
+                         year: currentEntry.year === 0 ? -1: currentEntry.year}); //Set to -1 to indicate invalid input
+
+        if( currentEntry.day > 0 && currentEntry.month > 0 && currentEntry.year > 0){
+            setDate(currentEntry);
+            setYearTabs([...yearTabs, currentEntry.year ]);
+            setChooseYear(false);
+            setCurrentEntry({day: 0, month: 0, year: 0});
+        }
+    }
+
+
+    function handleDayInput(day: number, ){
+    if( day < 0 || day > 31 || Number.isNaN(day)){
+        setCurrentEntry({ ...currentEntry, day: -1 });
+    }
+    else
+        setCurrentEntry({ ...currentEntry, day: day });
+}
+    function handleMonthInput(month: number){
+        if( month < 0 || month > 12 || Number.isNaN(month)){
+            setCurrentEntry({ ...currentEntry, month: -1 });
+        }
+        else
+            setCurrentEntry({ ...currentEntry, month: month });
+    }
+
+    function handleYearInput(year: number){
+        if( year < 0 || year > 9999 || Number.isNaN(year)){
+            setCurrentEntry({ ...currentEntry, year: -1 });
+        }
+        else
+            setCurrentEntry({ ...currentEntry, year: year });
+    }
+
 
     return (
         <div className={`bg-gray-100 h-full rounded-2xl p-5 flex flex-col items-start ${isFullSize ? 'w-2/5' : 'w-24'}`}>
@@ -14,21 +62,51 @@ function Menu() {
                 <ChevronLeftIcon onClick = {() => setIsFullSize(!isFullSize)} className={`w-4 h-4 text-gray-600 cursor-pointer ${isFullSize ? '' : 'rotate-180'}`} />
             </div>
             <div className = "w-full">
-                <YearTab isFullSize = {isFullSize} year = {2024} />
+                    {yearTabs.map(year => (
+                        <YearTab key={year} isFullSize={isFullSize} year={year} />
+                    ))}
             </div>
-            {chooseYear && <div onClick = {() => setChooseYear(false)} className = {"z-2 absolute inset-0 bg-gray-500 w-full h-full opacity-20"}></div>}
+            {chooseYear && <div onClick = {() => {setChooseYear(false); setCurrentEntry({day: 0, month: 0, year: 0})}} className = {"z-2 absolute inset-0 bg-gray-500 w-full h-full opacity-20"}></div>}
             {chooseYear &&
             <div className='font-zain text-gray-800 text-lg pt-4 absolute inset-1/2 w-1/2 h-1/2 -translate-x-1/2 -translate-y-1/2 bg-white z-3 rounded-md flex flex-col items-center '>
-                <div className='w-fit text-nowrap'> Add an entry manually</div>
-                <div className = "font-zain text-xl text-gray-800 m-4 w-2/3 px-4 h-10 bg-gray-100 opacity-90 flex flex-row items-center justify-between text-center rounded-md">
-                    <input className='w-12 text-gray-800 cursor-pointer text-center' placeholder = "dd"/>
+                <div className='w-fit text-nowrap'> Add a n entry manually</div>
+                <form className = "font-zain text-xl text-gray-800 m-4 w-2/3 h-10 bg-gray-100 opacity-90 flex flex-row items-center justify-between text-center rounded-md"
+                    onSubmit={handleSubmit}
+                >
+                    <div className={'h-full w-0 opacity-0'}></div>
+                    <input className={`${currentEntry.day == -1? "bg-red-200" : ""} w-12 text-gray-800 cursor-pointer text-center`}
+                        type = "number"
+                        min = {1}
+                        max = {31}
+                        placeholder = "dd"
+                        onChange={e => {
+                            e.target.value = e.target.value.slice(0,2); //Ensure only two digits are entered
+                            handleDayInput(Number(e.target.value))
+                        }}
+                    />
                     <div>/</div>
-                    <input className='w-12 text-gray-800 cursor-pointer text-center' placeholder = "mm"/>
+                    <input className={`${currentEntry.month == -1? "bg-red-200" : ""} w-12 text-gray-800 cursor-pointer text-center`}
+                        placeholder = "mm"
+                        type = "number"
+                        min = {1}
+                        max = {12}
+                        onChange={e => {e.target.value = e.target.value.slice(0,2); //Ensure only two digits are entered
+                                       handleMonthInput(Number(e.target.value))}}
+                    />
                     <div>/</div>
-                    <input className='w-12 text-gray-800 cursor-pointer text-center' placeholder = "yyyy"/>
-                    {/* <div className='text-gray-400 cursor-pointer'>mm</div>
-                    <div className='text-gray-400 cursor-pointer'>yyyy</div> */}
-                </div>
+                    <input className={`${currentEntry.year == -1? "bg-red-200" : ""} w-12 text-gray-800 cursor-pointer text-center`}
+                        type = "number"
+                        min = {1}
+                        max = {9999}
+                        placeholder = "yyyy"
+                        onChange={e => { e.target.value = e.target.value.slice(0,4); //Ensure only four digits are entered
+                                        handleYearInput(Number(e.target.value))
+                        }}
+                    />
+                    <button className='h-full w-12 bg-green-200 cursor-pointer flex items-center justify-center rounded-r-md'
+                        type = "submit"
+                    >{'>'}</button>
+                </form>
             </div>
             }
         </div>
--- src/react/YearTab.tsx
+++ src/react/YearTab.tsx
@@ -1,7 +1,7 @@
 function YearTab({ year, isFullSize } :{year: number; isFullSize: boolean}) {
 
   return (
-      <div className={`font-zain bg-green-200 w-full h-12 rounded-md flex items-center ${isFullSize? "p-5" : "p-2 justify-center"} `}>
+      <div className={`font-zain bg-green-200 w-full h-12 rounded-md flex items-center mb-2 max-w-96 ${isFullSize? "p-5" : "p-2 justify-center"} `}>
        {isFullSize? year : year.toString().slice(-2)}
       </div>
   )
